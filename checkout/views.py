6bca46bb96507718983b831aa0caa86acec59935
Amy Richardson <amyloumc@gmail.com>
2023-07-28 14:54:26 +0000

    add: basic checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,7 @@ from basket.contexts import basket_contents
 
 import stripe
 
+
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
